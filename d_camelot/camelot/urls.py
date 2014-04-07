856d30ceffea8096141a70c29c02fcf3d5046414
tecnomics <askukunnath@gmail.com>
2014-04-07 17:31:46 +0530

    project setting

---
+++ d_camelot/camelot/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns,include,url
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+
+from views import (HomeView,IndexView)
+
+urlpatterns = patterns('',
+	url(r'^$', HomeView.as_view(), name="home"),
+    url(r'^index/$', IndexView.as_view(), name="index"),
+	)
--- d_camelot/camelot/views.py
+++ d_camelot/camelot/views.py
@@ -1,1 +1,27 @@
-# Create your views here.
+import simplejson
+import urllib2
+import ast
+from datetime import datetime, timedelta
+from django.shortcuts import get_object_or_404, render
+from django.views.generic.base import View
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.db import IntegrityError
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.core.mail import send_mail, BadHeaderError, EmailMessage, EmailMultiAlternatives, mail_admins
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login, logout
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'index.html',context)
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html', {})
