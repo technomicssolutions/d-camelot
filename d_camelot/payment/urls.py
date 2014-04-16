505207543d777aa80bd8c188e6711c68e19aeced
technomics <remya@technomicssolutions.com>
2014-04-16 12:51:13 +0530

    slid estyles

---
+++ d_camelot/payment/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns,include,url
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+
+
+urlpatterns = patterns('',
+)
--- d_camelot/shop/models.py
+++ d_camelot/shop/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from camelot.models import Dates
+from inventory.models import Brand
+
+SHOPE_TYPE = (
+    ('baby', 'Baby'),
+    ('ladies', 'Ladies'),
+    ('mens', 'Mens'),
+    ('textiles', 'Textiles'),
+)
+class Shop(Dates):
+    shop_name = models.CharField('Name', max_length=100, unique=True)
+    shop_type = models.CharField('Shop Type', max_length=10, choices=SHOPE_TYPE)
+    slider_image = models.ImageField('Slider Image', upload_to="uploads/shop/")
+    logo = models.ImageField('Logo', upload_to="uploads/shop/")
+    brands = models.ManyToManyField(Brand)
+
+    class Meta:
+        verbose_name = 'Shop'
+        verbose_name_plural = 'Shops'
+
+    def __unicode__(self):
+        return self.shop_name
